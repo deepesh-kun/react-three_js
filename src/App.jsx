0dfe6b121ab855af45fd1cbb1950e911d8a88d61
deepesh <wrk.deepesh@gmail.com>
2024-07-08 21:50:34 +0530

    Play with different geometries,  render multiple

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { Canvas } from '@react-three/fiber'
-import Box from './Box.jsx'
+import Polyhedron from './Polyhedron.jsx'
 import { OrbitControls, Stats } from '@react-three/drei'
+import * as THREE from 'three'
 
 const App = () => {
+  const geometry = [
+    new THREE.BoxGeometry(),
+    new THREE.SphereGeometry(0.785398),
+    new THREE.DodecahedronGeometry(0.785398)
+  ]
+
   return (
-    <Canvas camera={{ position: [0, 0, 2] }}>
-      <Box position={[-0.75, 0, 0]} name="A" />
-      <Box position={[0.75, 0, 0]} name="B" />
+    <Canvas camera={{ position: [0, 0, 3] }}>
+      <Polyhedron position={[-0.75, -0.75, 0]} geometry={geometry} />
+      <Polyhedron position={[0.75, -0.75, 0]} geometry={geometry} />
+      <Polyhedron position={[-0.75, 0.75, 0]} geometry={geometry} />
+      <Polyhedron position={[0.75, 0.75, 0]} geometry={geometry} />
       <Stats />
       <OrbitControls />
       <axesHelper args={[3]} />
